dbc621cbea05b4669becd420473e265ccfbb13d0
leauzu <73183387+leauzu@users.noreply.github.com>
2026-05-26 03:04:36 +0700

    Delete gg directory

--- gg/balance.js
+++
@@ -1,6 +0,0 @@
-import { send, clearCookie } from './_lib.js';
-
-export default async function handler(req, res) {
-  if (req.method !== 'POST') return send(res, 405, { error: 'Method not allowed' });
-  return send(res, 200, { ok: true }, { 'Set-Cookie': clearCookie() });
-}
