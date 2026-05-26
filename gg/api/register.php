52a64675d3bb8cc0f71f758a58330d5acf384eaf
leauzu <73183387+leauzu@users.noreply.github.com>
2026-05-27 00:52:21 +0700

    Update register.php

--- gg/api/register.php
+++ gg/api/register.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 require __DIR__ . '/_lib.php';
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') send_json(405, ['error' => 'Method not allowed']);
 try {
